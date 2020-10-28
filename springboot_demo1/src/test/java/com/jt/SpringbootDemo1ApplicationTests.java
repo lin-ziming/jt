352f85a2835926e8f2cc9f11b8fcdb0c9663e38e
linziming <871440130@qq.com>
2020-10-28 21:06:51 +0800

    Initial commit

---
+++ springboot_demo1/src/test/java/com/jt/SpringbootDemo1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest //
+class SpringbootDemo1ApplicationTests {
+
+    //在测试
+
+    @Test
+    void contextLoads() {
+    }
+
+}
